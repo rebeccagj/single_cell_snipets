e916af555d5fe912415fc6978b0e3d273835ca22
Daniel Bunis <daniel.bunis@ucsf.edu>
2019-02-26 10:08:05 -0800

    Update auto-generate-tsne-plots.R
    Updated the for loops to sapplys, and pc input of 1:20 to a variable that only needs to be set once.  Also caught an instance of your old object name

--- auto-generate-tsne-plots.R
+++ auto-generate-tsne-plots.R
@@ -1,9 +1,11 @@
 #assumes normal pre-processing of Seurat object up "Cluster the cells" step of http://bit.ly/pbmc3k_tutorial
 #portions of this code from/inspired by http://bit.ly/ucdavis_scRNA_wksp_5
 
+pcs.use <- 1:20 #use of variable ensures the same PCs will be used for clustering and tsne
+
 seurat_object_var <- FindClusters(object = seurat_object_var, 
                              reduction.type = "pca", 
-                             dims.use = 1:20, #must be the same number specified in RunTSNE()
+                             dims.use = pcs.use,
                              resolution = seq(0.1,1,0.1), 
                              print.output = 0, 
                              save.SNN = TRUE,
@@ -17,11 +19,11 @@ length(rownames(res_dataframe)) #add this value to the interior for loop of the 
 
 #before running the below, amend x in 1:__ value
 setwd("~/") #dir to output all these pngs
-for (perp in seq(30,110,20)){ #sets "perp" to each of the values specified in seq()
-  seurat_object_var <- RunTSNE(object = seurat_object_var, dims.use = 1:20, do.fast = TRUE, perplexity = perp) #alters perp for each of the different previously computed res's; ensure dims.use = the variable used in FindClusters()
-  for (x in 1:10) {
+sapply(seq(30,110,20), function(perp){ #sets "perp" to each of the values specified in seq()
+  seurat_object_var <- RunTSNE(object = seurat_object_var, dims.use = pcs.use, do.fast = TRUE, perplexity = perp) #alters perp for each of the different previously computed res's; ensure dims.use = the variable used in FindClusters()
+  sapply(1:10, function(x) {
    png(paste0("tSNE-perp_",perp,"-",res_dataframe$res[x],".png"), width = 700, height = 600) #creates a blank file titled with the perp and resolution number
-   TSNEPlot(object = D1_G1_e11_5, group.by = res_dataframe$res[x], do.label = T, pt.size=1, plot.title = paste0("tSNE: perp ", perp, ", ", res_dataframe$res[x])) #plots the specific tsne with specific res/perp
+   TSNEPlot(object = seurat_object_var, group.by = res_dataframe$res[x], do.label = T, pt.size=1, plot.title = paste0("tSNE: perp ", perp, ", ", res_dataframe$res[x])) #plots the specific tsne with specific res/perp
    dev.off() #saves the photo
  }
 }
